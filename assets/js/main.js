e31c1bcc55b1d940123af731e70eeda6aba19441
KCroom03 <krc230003@utdallas.edu>
2025-04-29 16:01:22 +0000

    Paper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,1 @@
 // main.js
-document.addEventListener("DOMContentLoaded", () => {
-    const danceElement = document.querySelector(".description r");
-    const imageElement = document.querySelector(".overlay-img img");
-  
-    danceElement.addEventListener("mouseover", () => {
-      imageElement.src = "./img/cat.jpg";
-    });
-  
-    danceElement.addEventListener("mouseout", () => {
-      imageElement.src = "./img/normaldog.jpg";
-    });
-  });
