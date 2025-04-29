87a914d3afd79095245e4ac8ea37581e817eea04
KCroom03 <krc230003@utdallas.edu>
2025-04-29 15:48:46 +0000

    Edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 // main.js
-
-console.log("Hello, World!");
+document.addEventListener("DOMContentLoaded", () => {
+    const danceElement = document.querySelector(".description r");
+    const imageElement = document.querySelector(".overlay-img img");
+  
+    danceElement.addEventListener("mouseover", () => {
+      imageElement.src = "./img/cat.jpg";
+    });
+  
+    danceElement.addEventListener("mouseout", () => {
+      imageElement.src = "./img/normaldog.jpg";
+    });
+  });
